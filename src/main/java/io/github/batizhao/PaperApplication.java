0e206797b4da75db8f43780a4480bb1a268e4e60
Bati Zhao <zhaobati@gmail.com>
2016-09-28 14:41:26 +0800

    Spring boot init.

---
+++ src/main/java/io/github/batizhao/PaperApplication.java
@@ -0,0 +1,12 @@
+package io.github.batizhao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaperApplication.class, args);
+	}
+}
