2fbf383b4ec47b5d6f8ddc95e3ca959f4be5db50
Bati Zhao <zhaobati@gmail.com>
2020-03-20 19:41:06 +0800

    gateway.

---
+++ paper-gateway/src/main/java/me/baitzhao/gateway/PaperGatewayApplication.java
@@ -0,0 +1,17 @@
+package me.baitzhao.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author batizhao
+ * @since 2020/3/20
+ */
+@SpringBootApplication
+public class PaperGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaperGatewayApplication.class, args);
+    }
+
+}
