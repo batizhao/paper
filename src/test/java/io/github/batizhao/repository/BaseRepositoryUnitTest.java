6f7c4ea68d0e98d631e6f5dc0489d9caf76291c8
Bati Zhao <zhaobati@gmail.com>
2020-04-07 18:29:36 +0800

    @DataJpaTest

--- src/test/java/io/github/batizhao/repository/BaseRepositoryUnitTest.java
+++ src/test/java/io/github/batizhao/repository/BaseRepositoryUnitTest.java
@@ -1,6 +1,7 @@
 package io.github.batizhao.repository;
 
 import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -10,6 +11,6 @@ import org.springframework.test.context.junit4.SpringRunner;
  */
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@DataJpaTest
 public abstract class BaseRepositoryUnitTest {
 }
